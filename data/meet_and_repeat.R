242b67ffc9c4251b861f71745fc7b1a03634d4a0
David Rosson <david@rosson.au>
2023-12-06 15:27:58 +0200

    Add wrangling script for Week 6

---
+++ data/meet_and_repeat.R
@@ -0,0 +1,28 @@
+# Name: David Rosson
+# Date: 06.12.2023
+# File description: R script for Assignment 6
+# Data source: MABS4IODS (Part VI) by Vehkalahti and Everitt (2019)
+
+### Step 1 ###
+# Load data
+
+### Step 2 ###
+# "Convert the categorical variables of both data sets to factors"
+
+### Step 3 ###
+
+# "Convert the data sets to long form"
+
+# "Add a week variable to BPRS"
+
+# "Add a time variable to RATS"
+
+### Step 4 ###
+
+# "Compare new data with wide form"
+
+# "Check the variable names"
+
+# "View the data contents and structures"
+
+# "Create some brief summaries of the variables"
